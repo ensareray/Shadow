1ce9266c99a7e6d8093c0c59237d6a481c2153ef
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-05-09 02:26:23 +0300

    Enemy adding

---
+++ Assets/Scripts/Enemy/EnemyRoamingController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRoamingController : MonoBehaviour
+{
+    [SerializeField]Transform[] roamingPoints;
+    [SerializeField] NavMeshAgent navAgent;
+    [SerializeField]int currentRoamingPoint = 0;
+    bool walking;
+
+    void Start()
+    {
+        walking = true;
+    }
+    void Update()
+    {
+        while(walking == true)
+        {
+            Roam();
+        }
+    }
+    void RoamToNextPoint()
+    {
+        currentRoamingPoint++;
+        if(currentRoamingPoint >= roamingPoints.Length)
+        {
+            currentRoamingPoint = 0;
+        }
+    }
+    void Roam()
+    {
+        navAgent.destination = roamingPoints[currentRoamingPoint].position;
+        navAgent.isStopped = false;
+    }
+}
